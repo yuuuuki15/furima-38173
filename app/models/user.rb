e7e9cff5459bffc8fc513a5e56c9536f0baadf20
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-06-25 17:06:57 +0900

    ヘッダーからそれぞれのページへの移遷を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: "should contain both letters and numbers"}
     validates :name, presence: true
-    with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください' } do
+    with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'should be full-width characters' } do
       validates :last_name_kanji
       validates :first_name_kanji
     end
-    with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'に全角カタカナを使用してください' } do
+    with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'should be full-width katakana' } do
       validates :last_name_kana
       validates :first_name_kana
     end

9cd8eb001b06cec2dd7e8c551d3c2757182e7455
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-06-25 17:21:03 +0900

    rubocop実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: "should contain both letters and numbers"}
-    validates :name, presence: true
-    with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'should be full-width characters' } do
-      validates :last_name_kanji
-      validates :first_name_kanji
-    end
-    with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'should be full-width katakana' } do
-      validates :last_name_kana
-      validates :first_name_kana
-    end
-    validates :birthday, presence: true
+  validates :password,
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: 'should contain both letters and numbers' }
+  validates :name, presence: true
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'should be full-width characters' } do
+    validates :last_name_kanji
+    validates :first_name_kanji
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'should be full-width katakana' } do
+    validates :last_name_kana
+    validates :first_name_kana
+  end
+  validates :birthday, presence: true
 end

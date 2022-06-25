8ed4c5804f1c7dd132bab92bbcec8cdcb50961e2
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-06-25 16:09:12 +0900

    viewの作成とモデルのバリデーション
    rails g devise:views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,15 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    validates :name, presence: true
+    with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'に全角文字を使用してください' } do
+      validates :last_name_kanji
+      validates :first_name_kanji
+    end
+    with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'に全角カタカナを使用してください' } do
+      validates :last_name_kana
+      validates :first_name_kana
+    end
+    validates :birthday, presence: true
 end

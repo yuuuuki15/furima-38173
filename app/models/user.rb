6e5cef666332fa264311d772e518ba5188adb2a6
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-06-25 18:30:19 +0900

    Userモデル単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,11 @@ class User < ApplicationRecord
   validates :password,
             format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message: 'should contain both letters and numbers' }
   validates :name, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'should be full-width characters' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'should contain only full-width characters' } do
     validates :last_name_kanji
     validates :first_name_kanji
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'should be full-width katakana' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: 'should contain only full-width katakana' } do
     validates :last_name_kana
     validates :first_name_kana
   end

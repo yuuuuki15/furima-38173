c148c23b5714f5cf9b219232c2c9b59f2a65bcf2
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-06-27 15:50:09 +0900

    ストロングパラメーターの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,17 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-  
+
   def index
   end
 
   def new
+  end
+
+  def create(items_parameter)
+  end
+
+  private
+  def items_parameter
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :fee_id, :prefecture_id, :delivery_date_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   validates :image, presence: true
   validates :name, presence: true

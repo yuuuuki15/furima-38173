1290e70794d0e43fbb418870134fb8dc9e73d18d
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-06-27 15:38:02 +0900

    アソシエーションとバリデーションの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  belongs_to :user
+
+  validates :image, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :status_id , presence: true
+  validates :fee_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_date_id, presence: true
+  validates :price, presence: true
+  validates :user, presence: true, foreign_key: true
 end

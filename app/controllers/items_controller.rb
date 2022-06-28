993aa83000151c32b6f686e143ec69795d905fe3
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-06-28 20:44:06 +0900

    before_actionの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :destroy]
   before_action :redirect_to_index, only: [:edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 

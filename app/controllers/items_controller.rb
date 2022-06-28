bd13cd66bd4a381aa421b1a19fef99ba88ed39a5
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-06-28 18:09:41 +0900

    商品編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :redirect_to_index, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -22,10 +23,29 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(items_parameter)
+      redirect_to item_path(@item)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def items_parameter
     params.require(:item).permit(:name, :description, :category_id, :status_id, :fee_id, :prefecture_id, :delivery_date_id,
                                  :price, :image).merge(user_id: current_user.id)
+  end
+
+  def redirect_to_index
+    if current_user != Item.find(params[:id]).user
+      redirect_to root_path
+    end
   end
 end

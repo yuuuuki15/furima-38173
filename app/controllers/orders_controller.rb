93fd745e217733397a9a0723c28736e677fa480f
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-06-29 14:36:04 +0900

    orders controllerの作成
    間違えてaddresses controllerを作ってしまった

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,5 @@
+class OrdersController < ApplicationController
+  def index
+    @order = Order.new
+  end
+end
--- spec/helpers/orders_helper_spec.rb
+++ spec/helpers/orders_helper_spec.rb
@@ -1,15 +1,15 @@
 require 'rails_helper'
 
 # Specs in this file have access to a helper object that includes
-# the AddressesHelper. For example:
+# the OrdersHelper. For example:
 #
-# describe AddressesHelper do
+# describe OrdersHelper do
 #   describe "string concat" do
 #     it "concats two strings with spaces" do
 #       expect(helper.concat_strings("this","that")).to eq("this that")
 #     end
 #   end
 # end
-RSpec.describe AddressesHelper, type: :helper do
+RSpec.describe OrdersHelper, type: :helper do
   pending "add some examples to (or delete) #{__FILE__}"
 end

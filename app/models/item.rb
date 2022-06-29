078f26d62f2b58fb1badacb0f5a692e79616c5f2
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-06-29 20:49:56 +0900

    sold outの表示を実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,6 +7,7 @@ class Item < ApplicationRecord
   belongs_to :delivery_date
   belongs_to :user
   has_one_attached :image
+  has_one :order
 
   validates :image, presence: true
   validates :name, presence: true

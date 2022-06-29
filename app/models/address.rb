4524263104c128acd399e3289790bb7f46e87ef4
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-06-29 14:11:45 +0900

    アソシエーションとバリデーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,9 @@
 class Address < ApplicationRecord
+  belongs_to :order
+
+  validates :postal_code, presence: true
+  validates :prefecture_id, presence: true
+  validates :city, presence: true
+  validates :house_number, presence: true
+  validates :phone_number, presence: true
 end

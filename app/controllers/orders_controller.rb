149ece4d36f1b3ee6c97fea79725db5fa9578160
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-06-29 18:59:07 +0900

    tokenをストロングパラメーターにセットした
    tokenはキー名ではなくmergeで記載しないといけない

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,7 +19,7 @@ class OrdersController < ApplicationController
 
   private
   def order_params
-    params.require(:order_address).permit(:token, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
   def pay_item

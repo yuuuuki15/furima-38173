edc209ca88c8c572f45e114407fd8d0b6ff4711e
yuuuuki15 <105331140+yuuuuki15@users.noreply.github.com>
2022-06-30 20:06:17 +0900

    レビューの修正２

--- db/migrate/20220629050614_create_addresses.rb
+++ db/migrate/20220629050614_create_addresses.rb
@@ -6,7 +6,7 @@ class CreateAddresses < ActiveRecord::Migration[6.0]
       t.string :city, null: false
       t.string :house_number, null: false
       t.string :building_name
-      t.string :phone_number
+      t.string :phone_number, null: false
       t.references :order, null: false, foreign_key: true
       t.timestamps
     end
